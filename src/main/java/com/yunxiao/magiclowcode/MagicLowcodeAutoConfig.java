6eb04a2c9fca089c05e4d914989833a2b1b2cda9
LuoYunXiao <1829462342@qq.com>
2024-02-09 10:12:07 +0800

    添加默认创建数据表代码

--- src/main/java/com/yunxiao/magiclowcode/MagicLowcodeAutoConfig.java
+++ src/main/java/com/yunxiao/magiclowcode/MagicLowcodeAutoConfig.java
@@ -1,13 +1,36 @@
 package com.yunxiao.magiclowcode;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.ssssssss.magicapi.spring.boot.starter.MagicAPIAutoConfiguration;
 
 @Configuration
 @ComponentScan(basePackages = "com.yunxiao.magiclowcode.custom")
 @RequiredArgsConstructor
-public class MagicLowcodeAutoConfig {
+@AutoConfigureBefore(MagicAPIAutoConfiguration.class)
+@AutoConfigureAfter(DataSourceAutoConfiguration.class)
+public class MagicLowcodeAutoConfig implements InitializingBean {
 
-
+    final JdbcTemplate jdbcTemplate;
+    void createTable() {
+        var sql = """
+                CREATE TABLE IF NOT EXISTS `magic_api_file_v2` (
+                  `file_path` varchar(512) NOT NULL,
+                  `file_content` mediumtext,
+                  PRIMARY KEY (`file_path`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+                                
+                """;
+        jdbcTemplate.execute(sql);
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        createTable();
+    }
 }
